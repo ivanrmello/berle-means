264e7bcdc2bdc5f22e8e556e63c7038fea36c280
Ivan Ribeiro Mello <ivan.ribeiro.mello@gmail.com>
2021-07-17 12:24:46 -0300

    feat: :zap: Updating some utils scripts.

--- quantitative-methods/01_Lanitt/web_scraping_jobs/Combining all csv inside a folder into one df.py
+++ quantitative-methods/01_Lanitt/web_scraping_jobs/Combining all csv inside a folder into one df.py
@@ -8,15 +8,15 @@ Created on Fri May 14 13:37:06 2021
 import pandas as pd
 import glob
 
-path = r'C:\Users\ivanr\Documents\86 - Webscraping Jobs\Catho Online\COTEC & EFG' # use your path
+path = r'C:\Users\ivanr\Documents\berle-means\accounting-theory\crawled_data\accounting' # use your path
 all_files = glob.glob(path + "/*.csv")
 
 li = []
 
 for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=0, sep='|')
+    df = pd.read_csv(filename, index_col=None, header=0, sep='|', encoding='UTF-16')
     li.append(df)
 
 frame = pd.concat(li, axis=0, ignore_index=True)
 
-frame.to_csv(r'C:\Users\ivanr\Documents\86 - Webscraping Jobs\Catho Online\COTEC & EFG\cotec_efg_catho_online_all_jobs.csv', index = False, sep ='|', encoding='utf-8')
+frame.to_excel('combined.xlsx', index=False)
